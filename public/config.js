6612ac42eff099e45bb8ae13787f792b0c8c8967
Dean @ Skool <dbjones3@students.wcpss.net>
2024-09-03 09:35:10 -0400

    fsefs

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "https://tvproxy.skool.workers.dev","mathwork1-learning.netlify.app","english2-worksheets.netlify.app","teachers-worksheets1.netlify.app",
+  VITE_CORS_PROXY_URL: "https://mathwork1-learning.netlify.app",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZmRiMzQ5MDY1YTM0NjAwM2IwMzY2ODgxYWY2ZWFhMSIsIm5iZiI6MTcyNTMzMjQ2MS42ODkzODEsInN1YiI6IjY0ZGUxNGUzYTNiNWU2MDBlMjljOTkyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xzGv4_I2k5yci4b0wOYb_Ovnl3Ciil4UGHIzxQYRcRk",
